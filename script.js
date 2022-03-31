5f554cd00cde0e895fea89150388f50c786070d7
Damien <damien.lebre88@gmail.com>
2022-03-31 16:26:19 +0200

    change class grid-item

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// function changeClass(){
-// let name = document.getElementById("grid-item")
-// name.className="cardFace"
-// }
 
 let card = document.getElementById('grid-item');
       
 card.addEventListener("click", function(e) {
     card.className = "cardReturn"
 });
+
